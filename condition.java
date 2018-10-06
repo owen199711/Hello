d8373c70f9476c45fd1c9fb692f03ea6c5ded780
owen199711 <34670345+owen199711@users.noreply.github.com>
2018-10-06 16:13:13 +0800

    Add files via upload

---
+++ condition.java
@@ -0,0 +1,27 @@
+package com.jia.bean;
+
+public class condition {
+	private String pname;
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getIsHot() {
+		return isHot;
+	}
+	public void setIsHot(String isHot) {
+		this.isHot = isHot;
+	}
+	public String getCid() {
+		return cid;
+	}
+	public void setCid(String cid) {
+		this.cid = cid;
+	}
+	private String isHot;
+	private String cid;
+	
+
+}
